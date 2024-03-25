4ca522576afd1189baa4ab682e2a9e3a1ac422eb
skitamur <skitamur@redhat.com>
2024-03-26 04:37:58 +0900

    fix

--- rosa-skeleton/terraform/output.tf
+++ rosa-skeleton/terraform/output.tf
@@ -13,3 +13,8 @@ output "azs" {
 output "aws_region" {
   value = local.aws_region
 }
+
+output "subnet_ids" {
+  #value = module.vpc.private_subnets
+  value =  join(",", concat(module.vpc.public_subnets, module.vpc.private_subnets))
+}
